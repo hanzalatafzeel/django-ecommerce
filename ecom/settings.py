8cdba62ea9f38499ce22493e2eb8a65eac12a39f
Hanzala <hanzalatafzeel44@gmail.com>
2024-07-28 20:55:18 +0530

    added staticfiles

--- ecom/settings.py
+++ ecom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0xd+ite!5oc95_+ium1#uk@@t(9q++7g^bpjmt&8(g@6co=^x(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh' , '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "img/"
 MEDIA_ROOT = BASE_DIR/'media'
