9f6ce48a57c5ce47bbfd45adb5f1f44d538ffd79
Hanzala <hanzalatafzeel44@gmail.com>
2024-07-30 12:23:16 +0530

    added newArrival page

--- djecom/urls.py
+++ djecom/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
+    path('newArrivals', views.newArrivals , name="newArrivals")
 ]
--- djecom/views.py
+++ djecom/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request,'index.html')
+
+def newArrivals(request):
+    return render(request,'newArrivals.html')
